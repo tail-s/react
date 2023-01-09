fefc7027d612ed8b6a017cf55465eb4bc26fe61b
Oliverslife <voise.ksing@gmail.com>
2023-01-10 00:00:15 +0900

    23.01.10

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
 import Expense from './components/Expenses/Expense';
 
 const App = () => {
@@ -26,8 +29,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      <NewExpense></NewExpense>
       <Expense expenses={expenses}></Expense>      
     </div>
   );
