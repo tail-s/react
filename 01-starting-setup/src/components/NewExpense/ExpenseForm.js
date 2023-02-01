20abe6fe1ba1c8a98bfe87370171a2fab53f8f5d
Oliverslife <voise.ksing@gmail.com>
2023-02-02 01:26:28 +0900

    23.02.02

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
     const titleChangeHandler = (event) => {
-        console.log(event.target.value);
+        setEnteredTitle(event.target.value);
+    };
+
+    const amountChangeHandler = event => {
+        setEnteredAmount(event.target.value);
+    };
+
+    const dateChangehandler = event => {
+        setEnteredDate(event.target.value);
     };
 
     return (
@@ -16,7 +29,7 @@ const ExpenseForm = () => {
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0,01"></input>
+                    <input type="number" min="0.01" step="0,01" onChange={amountChangeHandler}></input>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
