47dfaaac97b1ff767c0f3d12d57afc485432f357
Oliverslife <voise.ksing@gmail.com>
2023-02-25 03:10:22 +0900

    23.02.25

--- 01-starting-setup/src/components/Expenses/Expense.js
+++ 01-starting-setup/src/components/Expenses/Expense.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem.js';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter.js';
+import ExpensesList from './ExpensesList.js';
+import ExpensesChart from './ExpensesChart';
 import './Expense.css';
 
 const Expense = (props) => {
@@ -16,20 +17,24 @@ const Expense = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    let expensesContent = <p>No expenses found.</p>;
+    // let expensesContent = <p>No expenses found.</p>;
 
-    if(filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-        ));
-    }
+    // if(filteredExpenses.length > 0) {
+    //     expensesContent = filteredExpenses.map((expense) => (
+    //         <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+    //     ));
+    // };
 
     return (
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {expensesContent}
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpensesList items={filteredExpenses} />
 
+
+
+                {/* {expensesContent} */}
 
                 {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
                 {filteredExpenses.length > 0 && (
