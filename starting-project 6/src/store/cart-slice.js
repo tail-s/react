fb0e67460d69e483077c1f78aa48bb295fa34ce6
Oliverslife <voise.ksing@gmail.com>
2023-03-13 23:17:24 +0900

    23.03.13

--- starting-project 6/src/store/cart-slice.js
+++ starting-project 6/src/store/cart-slice.js
@@ -8,7 +8,22 @@ createSlice({
     },
     reducers: {
         addItemToCart(state, action) {
-            const item = action.payload;
+            const newItem = action.payload;
+            const existingItem = state.itmes.find(itme => item.id === newItem.id);
+            if (!existingItem) {
+                state.itmes.push({
+                    itemId: newItem.id, 
+                    price: newItem.price, 
+                    quantity: 1, 
+                    totalPrice: newItem.price,
+                    name: newItem.title
+                });
+            } else {
+                existingItem.quantity++;
+                existingItem.totalPrice = existingItem.total + newItem.price;
+            }
+
+            }
             
         },
         removeItemFromCart() {},
