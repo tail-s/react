3a84b238acbb164a4634ed17b8f0aa5991f79047
Oliverslife <voise.ksing@gmail.com>
2023-03-19 03:35:49 +0900

    23.03.19

---
+++ starting-project_9_ts/basics.ts
@@ -0,0 +1,43 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number = 24;
+
+age = 12;
+
+let userName: string;
+
+userName = 'Max';
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+    name: string;
+    age: number;
+
+};
+
+person = {
+    name: 'Max',
+    age: 32
+}
+
+// person = {
+//     isEmployee: true
+// }
+
+let people: {
+    name: string,
+    age: number;
+}[];
