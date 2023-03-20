4c0a53d912f0483bf7270525bb1c63f97ef3921d
Oliverslife <voise.ksing@gmail.com>
2023-03-20 23:38:34 +0900

    23.03.20

--- react-ts-starting-project 9.5/src/components/TodoItem.tsx
+++ react-ts-starting-project 9.5/src/components/TodoItem.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import classes from './TodoItem.module.css';
 
-// const TodoItem: React.FC<{ text: string; onRemoveTodo: (event: React.MouseEvent) => void }> = (props) => {
-const TodoItem: React.FC<{ text: string; onRemoveTodo: () => void }> = (props) => {
+const TodoItem: React.FC<{ text: string; onRemoveTodo: (event: React.MouseEvent) => void }> = (props) => {
+// const TodoItem: React.FC<{ text: string; onRemoveTodo: () => void }> = (props) => {
     return <li className={classes.item} onClick={props.onRemoveTodo}>{props.text}</li>
 };
 
